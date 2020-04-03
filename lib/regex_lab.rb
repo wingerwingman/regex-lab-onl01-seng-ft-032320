4e2b6728c99a6a81a4e9e4c86546097fdc790841
Justin Winger <justinwinger4@gmail.com>
2020-04-03 16:35:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-word.match?(/\A[aeiouAEIOU]/)
+  word.match?(/\A[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/\bun\w+ing\b/)
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/[a-zA-Z]{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
