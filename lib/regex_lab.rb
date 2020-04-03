14b66b8d6548116bc9f4a81b4f42e3f687421d5a
Justin Winger <justinwinger4@gmail.com>
2020-04-03 16:26:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ word.match?(/\A[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/[?=un&&[?!ing]]/)
+text.scan(/?=[un&&[ing]]/)
 end
 
 def words_five_letters_long(text)
