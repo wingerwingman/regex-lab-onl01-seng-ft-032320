4289e3d7c71407d90ad0059baff286affc801515
Justin Winger <justinwinger4@gmail.com>
2020-04-03 16:45:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match?(/\bA-Z\w+!\b/)
+  text.match?(/^[A-Z].*\.$/)
 end
 
 def valid_phone_number?(phone)
