f0525bb4a7cb675cae6f9b1892008cf283228893
Justin Winger <justinwinger4@gmail.com>
2020-04-03 16:37:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def starts_with_a_vowel?(word)
   word.match?(/\A[aeiouAEIOU]/)
 end
@@ -7,6 +8,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
+  binding.pry
   text.scan(/\b\w{1,5}\b/)
 end
 
