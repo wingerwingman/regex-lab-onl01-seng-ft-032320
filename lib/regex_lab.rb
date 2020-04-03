43ad7864891ca4caaa52209a0c5e50716621513d
Justin Winger <justinwinger4@gmail.com>
2020-04-03 16:52:48 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  word.match?(/\A[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  # binding.pry
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match?(/^[A-Z].*\.$/)
 end
 
 def valid_phone_number?(phone)
-
+  # binding.pry
+  phone.match?(/^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
 end
